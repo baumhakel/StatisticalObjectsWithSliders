aa26124648e0d50710fe89dd559bc7516ed71e82
baumhakel <baumhakel@tugraz.at>
2026-03-11 14:49:49 +0100

    Compile with correct folders

--- myapp/compile_app.R
+++ myapp/compile_app.R
@@ -1,6 +1,8 @@
+# manually set wd to deploy correctly to github
+setwd("C:/Users/baujuc12/Nextcloud/lvs_betreuung/s26/statistik_fuer_info_vu/App_new/StatisticalObjectsWithSliders")
 shinylive::export(
-  appdir = ".",
-  destdir = "site",
+  appdir = "myapp",
+  destdir = "docs",
   pkgs = c("shiny", "ggplot2", "bslib", "munsell", "markdown") #  "PearsonDS", 
 )
 
