10fcf5ec5301b6245405360957e9b611fb81ba88
baumhakel <baumhakel@tugraz.at>
2026-04-13 09:54:42 +0200

    Move page to site, add packages

--- myapp/compile_app.R
+++ myapp/compile_app.R
@@ -2,12 +2,12 @@
 setwd("C:/Users/baujuc12/Nextcloud/lvs_betreuung/s26/statistik_fuer_info_vu/App_new/StatisticalObjectsWithSliders")
 shinylive::export(
   appdir = "myapp",
-  destdir = "docs",
-  pkgs = c("shiny", "ggplot2", "bslib", "munsell", "markdown"),
+  destdir = "site",
+  pkgs = c("shiny", "ggplot2", "bslib", "munsell", "commonmark", "gld", "shinylive", "Markdown"),
   template_params = list(title = "SOS",
                          base_url = "/StatisticalObjectsWithSliders/")
 )
 
 # and test
-httpuv::runStaticServer("docs", port = 8080)
+httpuv::runStaticServer("site", port = 8080)
 
